f428415fc6cf6de01f63d5ea2660747b91743129
David Mila <David Mila>
2016-12-17 21:54:05 -0500

    Bill Input eroor update to tip calculator

--- CodePathPrework.php
+++ CodePathPrework.php
@@ -6,17 +6,32 @@ echo '<body bgcolor="#ffdd99">';
 
 echo '<div id="myDiv" name="myDiv" title="CalculatorDiv" style="text-align:center; color: #0900C4; font-family: verdana;border: 1px solid black; width: 40%; border:10px solid green">';
 
+echo '<h1> My Tip Calculator </h1>';
+
+echo '<h1> Please enter the bill: </h1>';
+
 if($_SERVER["REQUEST_METHOD"] == "POST")
 {
 	$bill_entry = $_POST["bill_entry"];
 
 	$tip_percentage = $_POST["tip_percentage"];
 
-	echo '<h1> My Tip Calculator </h1>';
 
-	echo '<h1> Please enter the bill: </h1>';
+	$valuesAreReasonable = false;
+	
+	if(is_numeric($bill_entry) && $bill_entry > 0 && $tip_percentage != "")
+	{
+		$valuesAreReasonable = true;
+	}
 
-	echo '<h3 style="display:inline-block;">$</h3><input type="text" style="padding:12px 20px;margin:8px 0" value="'.$bill_entry.'" name="bill_entry" id="bill_entry">';
+	if($valuesAreReasonable)
+	{
+		echo '<h3 style="display:inline-block;">$</h3><input type="text" style="border: 2px solid blue;padding:12px 20px" value="'.$bill_entry.'" name="bill_entry" id="bill_entry">';
+	}
+	else
+	{
+		echo '<h3 style="display:inline-block;">$</h3><input type="text" style="border: 2px solid red;padding:12px 20px" value="'.$bill_entry.'" name="bill_entry" id="bill_entry">';	
+	}
 
 	echo '<h1> Tip percentage: </h1>';
 
@@ -33,13 +48,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
 	}
 
 	echo '<br> <br>';
-	$valuesAreReasonable = false;
-
-	if(is_numeric($bill_entry) && $bill_entry > 0 && $tip_percentage != "")
-	{
-		$valuesAreReasonable = true;
-	}
-
+	
 	if($valuesAreReasonable)
 	{
 		echo '<div id="mini_Div" name="mini_Div" title="Result Div" style="color: #0900C4; font-family: verdana;border: 1px solid black; width: 50%; border: 5px solid green; margin: 0 auto; text-align:center">';
@@ -61,9 +70,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
 
 else
 {
-	echo '<h1> My Tip Calculator </h1>';
-
-	echo '<h1> Please enter the bill: </h1>';
 
 	echo '<h3 style="display:inline-block;">$</h3><input type="text" style="padding:12px 20px;margin:8px 0" value="'.$bill_entry.'" name="bill_entry" id="bill_entry">';
 
